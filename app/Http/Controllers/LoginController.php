fb9b4032f13324b2bb2259abcaa80dd7d63f1303
Ezeko <ezeko2017@gmail.com>
2020-06-24 03:51:27 +0100

    bug
    make logout work

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -39,6 +39,6 @@ class LoginController extends Controller
     public function logout () 
     {
         session()->flush();
-        return redirect(route('login'));
+        return redirect('/');
     }
 }
