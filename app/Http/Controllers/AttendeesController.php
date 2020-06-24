cc3895e84f6d64e0f5700b065a4e136b21de572c
Ezeko <ezeko2017@gmail.com>
2020-06-24 02:20:18 +0100

    feat
    -create add attendee to talk function
    -create attendance migration

--- app/Http/Controllers/AttendeesController.php
+++ app/Http/Controllers/AttendeesController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Attendance;
+use App\Talk;
 use Illuminate\Http\Request;
 use App\User;
 
@@ -37,5 +39,62 @@ class AttendeesController extends Controller
                 return view('register')->with(['issue' => $error, 'username' => $request->name, 'email' => $request->email]);
             }
 
+    }
+
+    //add attendee to talk
+    //create attendance
+    public function add(Request $request)
+    {
+        validator([
+            'attendee_id' => 'required',
+            'talk_id'  => 'required',
+        ]);
+        
+        //initailize
+        $attendee_id = $request->attendee_id;
+        $talk_id = $request->talk_id;
+
+        //check if logged in
+        if (session()->get('user_id')){
+            //continue adding
+            $check_attendee_id = User::find($attendee_id);
+
+            //check if attendee_id exist
+            if ($check_attendee_id){
+
+                //check talk id
+                $check_talk = Talk::find($talk_id);
+
+                if($check_talk){
+                    //add to attendance
+                    $attendance = new Attendance();
+                    $attendance->talk_id = $talk_id;
+                    $attendance->attendee_id = $attendee_id;
+
+                    $saved = $attendance->save();
+
+                    if ($saved){
+                        //return 201
+                        return response($content =['message' => 'Attendee added to talk successfully', 'data' => [], 'response'=> 'created'], 201);
+                    }else{
+                        //return 400
+                        return response($content =['message' => 'error occurred', 'data' => [], 'response'=> 'error'], 400);
+                    }
+                }else{
+                    //return 404 error
+                    return response($content =['message' => 'Bad request', 'data' => [], 'response'=> 'error'], 404);
+                }
+
+            }else{
+                //return 403 error
+                return response($content =['message' => 'Unauthorized attendee', 'data' => [], 'response'=> 'error'], 403);
+            }
+
+        }else{
+            //return 403 error
+            return response($content =['message' => 'Unauthorized request', 'data' => [], 'response'=> 'error'], 403);
+        }
+        
+        
     }
 }
